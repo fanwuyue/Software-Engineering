34ce64148eeba134c5a7c5866ebe369319500c1f
lizhisuao <lizhisuao123@gmail.com>
2020-05-01 15:04:27 +0800

    finish admin

--- html/static/js/base-js.js
+++ html/static/js/base-js.js
@@ -22,7 +22,7 @@ $().ready(function(){
 
   var working_year = document.getElementById("working_year");
   str = "";
-  for(var i = 1; i < 50; i++){
+  for(var i = 1; i <= 50; i++){
     str += "<option value=" + i + ">" + i + "</option>";
   }
   working_year.innerHTML = str;
