d2423c65290f76e00b915fea92a0b82d3125fb61
guzeming <1285052975@qq.com>
2020-05-01 16:41:57 +0800

    js first

--- html/static/js/signin.js
+++ html/static/js/signin.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
   var link='http://sw.womoe.top';
-// if($.cookie("username")!=null&&$.cookie("token")!=null){
-//   //alert('有cookie'+document.cookie);
-//   $.ajax({
-//     url: link+'/admin/verifyLogin',
-//     type: 'GET',
-//     xhrFields: {
-// 　　　　　　withCredentials: true
-// 　　　　}
-//   })
-//   .done(function(status){
-//     if(status=='success'){
-//       alert("校验成功");
-//     }else{
-//       alert('校验失败');
-//     }
-//   })
-// }
+if($.cookie("username")!=null&&$.cookie("token")!=null){
+  console.log('有cookie'+document.cookie);
+  $.ajax({
+    url: link+'/admin/verifyLogin',
+    type: 'GET',
+    xhrFields: {
+　　　　　　withCredentials: true
+　　　　}
+  })
+  .done(function(status){
+    if(status=='success'){
+      alert("校验成功");
+    }else{
+      alert('校验失败');
+    }
+  })
+}
 
   $("button").click(function(event) {
     /* Act on the event */
@@ -25,7 +25,7 @@ $(document).ready(function() {
     if(name==""||pwd==""){
       alert("用户名和密码不得为空");
     }else{
-  
+
     $.ajax({
       url: link+'/admin/login?username='+name+'&password='+pwd,
       type: 'GET'
@@ -37,7 +37,7 @@ $(document).ready(function() {
          $.cookie('username', name);
            $.cookie('token', t);
         // console.log(document.cookie);
-        //alert(document.cookie);
+        alert(document.cookie);
        }else{
          alert("账号或密码有误");
        }
