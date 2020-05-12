a40d1276e68f3947cec4cc396f527d051ade3de6
guzeming <1285052975@qq.com>
2020-05-12 16:49:35 +0800

    test2.2

--- html/static/js/base-js.js
+++ html/static/js/base-js.js
@@ -1,8 +1,7 @@
 $().ready(function(){
 
-  day.innerHTML = str;
-
   var working_year = document.getElementById("working_year");
+
   str = "";
   for(var i = 1; i <= 50; i++){
     str += "<option value=" + i + ">" + i + "</option>";
